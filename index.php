6e28345f95241ff2441ae2149c4c67b8fe68c974
Nikita <nikita_340@mail.ru>
2015-11-30 20:28:32 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,4 @@
 <a href = 'task1.php'>Task 1</a><br>
-<a href = 'task2.php'>Task 2</a>
+<a href = 'task2.php'>Task 2</a><br>
+<a href = 'task4.php'>Task 4</a><br>
+<a href = 'task5.php'>Task 5</a><br>
